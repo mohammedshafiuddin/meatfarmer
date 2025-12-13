8cd6b252479d7d2734b48dc679a3ba08cb5561dd
mohammedshafiuddin <mohammedshafiuddin54@gmail.com>
2025-12-13 22:41:43 +0530

    enh

--- packages/ui/src/components/google-sign-in.tsx
+++ packages/ui/src/components/google-sign-in.tsx
@@ -1,13 +1,3 @@
-const androidClientId = '241178308178-t33436b5agbcnsrs1o36jr3fh1s0g8i8.apps.googleusercontent.com'
-const iosClientId = '241178308178-59kajtcjos0anpricj37m5u2t1b957es.apps.googleusercontent.com'
-const androidDebugClientId = '241178308178-t33436b5agbcnsrs1o36jr3fh1s0g8i8.apps.googleusercontent.com';
-
-const desktopClientId = '241178308178-js4dorresvnfarpu3d05f3rluubsm3rt.apps.googleusercontent.com';
-const desktopClientSecret = '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y';
-
-const webClientId = '241178308178-olp71t2dq9fs09jrmcbhkr14db8mdhmt.apps.googleusercontent.com'
-const webClientSecret = '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';
-
 import * as React from 'react';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
