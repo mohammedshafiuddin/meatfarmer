d7518c323d48e269d8a1bc0df3bcde6e47c1e2cc
mohammedshafiuddin <mohammedshafiuddin54@gmail.com>
2025-12-13 22:39:45 +0530

    enh

--- apps/user-ui/src/components/google-sign-in.tsx
+++ apps/user-ui/src/components/google-sign-in.tsx
@@ -1,13 +1,3 @@
-const androidClientId = '241178308178-t33436b5agbcnsrs1o36jr3fh1s0g8i8.apps.googleusercontent.com'
-const iosClientId = '241178308178-59kajtcjos0anpricj37m5u2t1b957es.apps.googleusercontent.com'
-const androidDebugClientId = '241178308178-t33436b5agbcnsrs1o36jr3fh1s0g8i8.apps.googleusercontent.com';
-
-const desktopClientId = '241178308178-js4dorresvnfarpu3d05f3rluubsm3rt.apps.googleusercontent.com';
-const desktopClientSecret = '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y';
-
-const webClientId = '241178308178-olp71t2dq9fs09jrmcbhkr14db8mdhmt.apps.googleusercontent.com'
-const webClientSecret = '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';
-
 import * as React from 'react';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
@@ -27,69 +17,31 @@ WebBrowser.maybeCompleteAuthSession();
 export default function GoogleSignInPKCE() {
 
 
-  // 👇 use getDefaultReturnUrl or makeRedirectUri without useProxy
-  // const redirectUri = AuthSession.makeRedirectUri({
-  //   // Optional: explicitly define a custom scheme for standalone builds
-  //   scheme: 'freshyo',
-  //   path: ''
+
+
+  // const [request, response, promptAsync] = Google.useAuthRequest({
+  //   androidClientId: androidClientId,
+  //   iosClientId: iosClientId,
+  //   webClientId: webClientId,
+  //   redirectUri: 'https://www.freshyo.in/oauthredirect',
+  //   // redirectUri: AuthSession.makeRedirectUri({ scheme: 'freshyo', path: 'oauthredirect' }),
+  //   scopes: ["openid", "profile", "email"],
   // });
-  // console.log({redirectUri})
 
-
-  // const [request, response, promptAsync] = AuthSession.useAuthRequest(
-  //   {
-  //     clientId: `${webClientId}.apps.googleusercontent.com`,
-  //     redirectUri,
-  //     scopes: ['openid', 'profile', 'email'],
-  //     codeChallengeMethod: AuthSession.CodeChallengeMethod.S256,
-  //     responseType: 'code',
-  //   },
-  //   discovery
-  // );
-
-
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: androidClientId,
-    iosClientId: iosClientId,
-    webClientId: webClientId,
-    redirectUri: 'https://www.freshyo.in/oauthredirect',
-    // redirectUri: AuthSession.makeRedirectUri({ scheme: 'freshyo', path: 'oauthredirect' }),
-    scopes: ["openid", "profile", "email"],
-  });
-
-//   const handleGoogleSignIn = async () => { 
-//     const response = 
-//   }
-
-
-  React.useEffect(() => {
-    if (response?.type === "success") {
-      const { authentication } = response;
-      console.log("Access token:", authentication?.accessToken);
-    }
-  }, [response]);
 
 
   // React.useEffect(() => {
-  //   if (response?.type === 'success' && response.params.code) {
-  //     (async () => {
-  //       const tokenResponse = await AuthSession.exchangeCodeAsync(
-  //         {
-  //           clientId: `${webClientId}.apps.googleusercontent.com`,
-  //           code: response.params.code,
-  //           redirectUri,
-  //           extraParams: { code_verifier: request!.codeVerifier! },
-  //         },
-  //         discovery
-  //       );
-
-  //     })();
+  //   if (response?.type === "success") {
+  //     const { authentication } = response;
+  //     console.log("Access token:", authentication?.accessToken);
   //   }
   // }, [response]);
 
+
   return (
     <View style={{ marginTop: 100 }}>
-      <Button title="Sign in with Google" onPress={() => promptAsync()} />
+      <Button title="Sign in with Google" onPress={() => {}} />
+      {/* <Button title="Sign in with Google" onPress={() => promptAsync()} /> */}
     </View>
   );
 }
